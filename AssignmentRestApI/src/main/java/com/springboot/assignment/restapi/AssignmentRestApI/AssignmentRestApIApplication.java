a8eb357e43c80a602d11f7bce52f7a09fb444668
Lenovo <Lenovo@DESKTOP-TAHOKJB>
2022-07-08 20:01:28 +0530

    Rename the project Commit

--- AssignmentRestApI/src/main/java/com/springboot/assignment/restapi/AssignmentRestApI/AssignmentRestApIApplication.java
+++ AssignmentRestApI/src/main/java/com/springboot/assignment/restapi/AssignmentRestApI/AssignmentRestApIApplication.java
@@ -1,7 +1,10 @@
 package com.springboot.assignment.restapi.AssignmentRestApI;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 
 @SpringBootApplication
 public class AssignmentRestApIApplication {
