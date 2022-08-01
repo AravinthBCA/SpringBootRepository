ae5a51c8e2f795ef9ff3d0b9b9b086819645b1eb
Lenovo <Lenovo@DESKTOP-TAHOKJB>
2022-08-01 08:42:56 +0530

    Teacher Class Update Commit

--- ProxyExample/src/Teacher.java
+++ ProxyExample/src/Teacher.java
@@ -3,8 +3,8 @@ import java.util.Date;
 public class Teacher {
 	
 	public static void main(String[] args) {
-		Student student = new StudentProxy(new Attendance(new Date(),false));
-		student.attendSession();
+		DailySession dailySession = new StudentProxy(new Attendance(new Date(),true));
+		dailySession.attendSession();
 	}
 	
 }
